4da98a8f3e69b6faec31a1f4d283b3c48364db05
Flavio <flcable@gmail.com>
2017-05-06 09:42:22 -0300

    Implementação do controller

---
+++ WebApiCorreios/Controllers/CorreiosController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebApiCorreios.Models;
+
+namespace WebApiCorreios.Controllers
+{
+    public class CorreiosController : ApiController
+    {
+        public endereco end = new endereco();
+        
+
+
+        // GET: api/Correios
+        public endereco Get()
+        {
+            return end;
+        }
+
+        // GET: api/Correios/5
+        public endereco Get(string id)
+        {
+            var ws = new WSCorreios.AtendeClienteClient();
+            try
+            {
+                var resposta = ws.consultaCEP(id);
+                end = new endereco
+                {
+                    Logradouro = resposta.end,
+                    Complemento = resposta.complemento,
+                    Complemento2 = resposta.complemento2,
+                    Bairro = resposta.bairro,
+                    Cidade = resposta.cidade,
+                    UF = resposta.uf,
+                    CEP = resposta.cep,
+                    erro = false
+                };
+            }
+            catch (Exception erro)
+            {
+                end = new endereco
+                {
+                    Logradouro = erro.Message.ToString(),
+                    CEP = erro.HResult.ToString(),
+                    erro = true
+                };
+               
+            }
+            
+            return end;
+
+        }
+
+        // POST: api/Correios
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Correios/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Correios/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApiCorreios/Models/endereco.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiCorreios.Models
+{
+    public class endereco
+    {
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Complemento { get; set; }
+        public string Complemento2 { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+        public bool erro { get; set; }
+
+    }
+}
