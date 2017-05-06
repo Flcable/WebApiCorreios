4b79297a151d415d8eb48a788003ff6fdd3c3ee2
Flavio <flcable@gmail.com>
2017-05-06 11:25:54 -0300

    Pesquisar CEP Implementado porem co  erro a serem corridos

---
+++ WebApiCorreios/Controllers/pesquisaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiCorreios.Controllers
+{
+    public class pesquisaController : Controller
+    {
+        // GET: pesquisa
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
